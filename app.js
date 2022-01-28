f42e08b3f6cfcb8f62683d42ea6fb1c8bb313791
Edouard Depret <edouarddepret@gmail.com>
2022-01-28 12:21:15 +0100

    Version d'avant midi, modification des images pour rajouter des liens

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ let btnSubmit = undefined; // document.querySelector("#btnSubmit");
 let formElements = undefined; // document.querySelector("#studentForm").elements;
 let tableStudent = undefined; // document.querySelector("#tableStagiaires > tbody");
 
+let exportDataBtn = undefined;
+
 let divInfoStagiaire = undefined;
 
 console.log("in js");
 
 class Student {
   constructor(id, nom, prenom, email, etude, bio) {
-    this.id = Math.random().toString();
+    this.id = Math.round(Math.random().toString() * 1000000000);
     this.nom = nom;
     this.prenom = prenom;
     this.email = email;
@@ -27,65 +29,57 @@ const formFielsArray = ["nom", "prenom", "email", "etude", "bio"];
 
 ////////////////////////////
 
-
-
-
 let form2Student = (currentFormElt) => {
   const student = new Student();
-
   formFielsArray.forEach((aField) => {
     student[aField] = currentFormElt[aField].value;
   });
-
   return student;
 };
 
 class FormFieldValidator {
- 
-   static nomMinChar = 2;
-   static nomMaxChar = 50;
+  static nomMinChar = 2;
+  static nomMaxChar = 50;
 
-    static prenomMinChar = 2;
-    static prenomMaxChar = 50;
+  static prenomMinChar = 2;
+  static prenomMaxChar = 50;
 
-
+  static bioMinChar = 2;
+  static bioMaxChar = 50;
 
   static nomValidation(nom) {
-    // to do
-    console.log( (nom.length >= this.nomMinChar) )
-    console.log(this.nomMinChar)
-    return (nom.length >= this.nomMinChar) && (nom.length <= this.nomMaxChar );
+    nom = nom.replace(/\s/g, '');
+    return nom.length >= this.nomMinChar && nom.length <= this.nomMaxChar;
   }
 
   static prenomValidation(prenom) {
-    
-    return (prenom.length >= this.prenomMinChar) && (prenom.length<= this.prenomMaxChar );
+    prenom = prenom.replace(/\s/g, '');
+    return (
+      prenom.length >= this.prenomMinChar && prenom.length <= this.prenomMaxChar
+    );
   }
 
   static emailValidation(email) {
     const validateAnEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
+      return String(email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    };
     return validateAnEmail(email);
   }
 
-
   static etudeValidation(etude) {
-    
-    console.log(etude)
+    console.log(etude);
     return etude != "Choisir votre niveau d'étude";
   }
 
   static bioValidation(bio) {
-    
-    return true; //bio != "";
+    bio = bio.replace(/\s/g, '');
+    return bio.length >= this.bioMinChar && bio.length <= this.bioMaxChar;
+    //return true; //bio != "";
   }
-
-
 
   static validateAll(currentFormElt) {
     const notValidate = [];
@@ -105,28 +99,43 @@ class FormFieldValidator {
   }
 }
 
-let showIncorrectField = (incorrectFieldName) => {
+let showIncorrectField = (incorrectFieldName, currentFormElt) => {
   const incorrectFielMessage = {
-    nom: "veuillez saisir un nom correct",
-    prenom: "veuillez saisir un prenom correct",
-    email: "veuillez saisir un email correct",
-    etude: "veuillez choisir le niveau d'etude",
-    bio:   "bio !!!",
+    nom: "Veuillez saisir un nom correct",
+    prenom: "Veuillez saisir un prénom correct",
+    email: "Veuillez saisir un email correct",
+    etude: "Veuillez choisir le niveau d'étude",
+    bio: "Veuillez saisir une bio correcte",
   };
 
   formFielsArray.forEach((element) => {
-    document.querySelector("#" + element + "Err").innerText = incorrectFieldName.includes(element) ? 
-      incorrectFielMessage[element] : "";
+    const isInvalide = incorrectFieldName.includes(element) 
+    document.querySelector("#" + element + "Err").innerText =
+     isInvalide ? incorrectFielMessage[element] : "";
+
+     if(isInvalide){
+       currentFormElt[element].classList.remove("is-valid" );
+       currentFormElt[element].classList.add( "is-invalid" );
+     }else{
+       currentFormElt[element].classList.remove("is-invalid" );
+
+      if(incorrectFieldName.length != 0 ){
+        currentFormElt[element].classList.add("is-valid");
+      }else{
+        currentFormElt[element].classList.remove("is-valid" );
+      }
+       
+     }
+
+   
   });
 };
 
-
-let emptyForm = (currentFormElt) =>{
- formFielsArray.forEach((aField) => {
+let emptyForm = (currentFormElt) => {
+  formFielsArray.forEach((aField) => {
     currentFormElt[aField].value = "";
   });
-
-}
+};
 
 //////// ********* DataBase
 
@@ -179,23 +188,57 @@ class MySGBD {
   }
 
   update(params) {}
+
+  exporteData(){
+    this.data = this.getAll();
+
+    const rows = [ ["id", "nom", "prenom", "email", "etude", "bio"] ];
+    Object.getOwnPropertyNames(this.data).forEach((id) =>{
+      rows.push(  [id, this.data[id]["nom"], this.data[id]["prenom"], this.data[id]["email"], this.data[id]["etude"], this.data[id]["bio"]  ])
+    })
+
+    let csvContent = "data:text/csv;charset=utf-8,";
+
+    rows.forEach(function(rowArray) {
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+      });
+
+
+      var encodedUri = encodeURI(csvContent);
+      var link = document.createElement("a");
+      link.setAttribute("href", encodedUri);
+      link.setAttribute("download", "liste_stagiaires.csv");
+      document.body.appendChild(link); 
+      link.click();
+
+  }
+
 }
 
 ////////////////////////***** fin database
-
-
 
 function onClickSubmitForm(formElements) {
   const isValideForm = FormFieldValidator.validateAll(formElements);
 
   if (isValideForm.length == 0) {
     const student = form2Student(formElements);
-    emptyForm(formElements);
 
     //save new User
     currentDB.add(student);
+
+    emptyForm(formElements);
+    showIncorrectField(isValideForm, formElements);
+
+    var modalStagiaire = new bootstrap.Modal(
+      document.getElementById("modalStagiaire")
+    );
+
+    modalStagiaire.toggle()
+
+    
   } else {
-    showIncorrectField(isValideForm);
+    showIncorrectField(isValideForm, formElements);
   }
 }
 
@@ -203,7 +246,7 @@ function onClickSubmitForm(formElements) {
 
 let showCurentStudent = (aStudent) => {
   const info2show = `<p> <h4> ${aStudent.prenom} ${aStudent.nom}</h4> <br> <b>Email :  ${aStudent.email}</b><br> 
-    <b> Etudes faites :</b> ${aStudent.etude}<br> <b>Bio : </b>${aStudent.bio} </p>`;
+    <b> Études faites :</b> ${aStudent.etude}<br> <b>Bio : </b>${aStudent.bio} </p>`;
   divInfoStagiaire.innerHTML = info2show;
 };
 
@@ -252,9 +295,7 @@ let initStudentTable = () => {
 
   Object.getOwnPropertyNames(storedStudent).forEach((id) => {
     addRow2tableStudent(storedStudent[id]);
-
   });
-
 };
 
 /////////////////////// Initialisation
@@ -279,9 +320,21 @@ try {
   tableStudent = document.querySelector("#tableStagiaires > tbody");
   divInfoStagiaire = document.querySelector("#infoStagiaire");
 
-  
+  exportDataBtn = document.querySelector("#exportBtn")
+
+    exportDataBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    console.log("on btn export");
+
+    currentDB.exporteData();
+
+
+    
+  });
+
 
   initStudentTable();
-
   console.log("on list students");
 }
+
